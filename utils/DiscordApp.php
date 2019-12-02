62257ae127cb97e8a819ce9ffc317a338c9a1822
Josewowgame2888 <mistertec7777777@gmail.com>
2019-12-02 16:51:01 -0600

    json decode && curl

--- utils/DiscordApp.php
+++ utils/DiscordApp.php
@@ -5,8 +5,57 @@ class DiscordApp
     public $message;
     public $title;
 
-    public function sendPacket(): void
+    public function sendPacket(): bool
     {
-        
+        if($this->isValidMessage() && $this->isValidTitle() && $this->isValidHook())
+        {
+            $handle = [
+                'username' => $this->title,
+                'content' => $this->message
+            ];
+
+            $discord = curl_init($this->hook);
+            curl_setopt($discord, CURLOPT_HTTPHEADER, array('content-type: application/json'));
+            curl_setopt($discord, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($discord, CURLOPT_POSTFIELDS, json_encode($handle));
+            curl_setopt($discord, CURLOPT_RETURNTRANSFER, 1);
+            curl_exec($discord);
+
+            if(curl_getinfo($discord) === 204)
+            {
+                return true;
+            } else {
+                return false;
+            }
+            
+        }
+        return false;
+    }
+
+    private function isValidHook(): bool
+    {
+        if(strlen($this->hook) > 20 && !is_numeric($this->hook) && $this->hook !== null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function isValidTitle(): bool
+    {
+        if(strlen($this->title) > 4 && $this->title !== null && !is_numeric($this->title))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function isValidMessage(): bool
+    {
+        if(strlen($this->message) > 5 && $this->message !== null)
+        {
+            return true;
+        }
+        return false;
     }
 }
